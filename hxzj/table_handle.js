0a4e4f563e25fba2c65125ae17729a96c6a071d2
hibernake <hibernake@gmail.com>
2015-04-30 00:50:56 +0800

    energy
    加入了回能解析

---
+++ hxzj/table_handle.js
@@ -0,0 +1,28 @@
+﻿function set_col(col, value){
+	var table = document.getElementById('data');
+	for(var row in table.rows){
+		if(table.rows[row].cells && table.rows[row].cells[col]){
+			table.rows[row].cells[col].style.display=value
+		}
+	}
+}
+function hide_col(col){
+	set_col(col, 'none')
+}
+function show_col(col){
+	set_col(col, '')
+}
+function on_check(check, col){
+	if(check.checked){
+		show_col(col)
+	}
+	else{
+		hide_col(col)
+	}
+}
+
+function on_check_multi(check, cols) {
+	for(var i in cols){
+		on_check(check, cols[i])
+	}
+}
